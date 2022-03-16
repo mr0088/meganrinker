74c3494f61261187b4a9e27f5d0cbf9bc96fc38a
mr0088 <mr0088@mix.wvu.edu>
2022-03-16 11:55:05 -0400

    adding working contact form

---
+++ contact.php
@@ -0,0 +1,36 @@
+<?php
+$field_name = $_POST['Name'];
+$field_email = $_POST['E-Mail'];
+$field_message = $_POST['Message'];
+$field_subject = $_POST['Subject'];
+
+
+$mail_to = 'mr0088@mix.wvu.edu';
+$subject = 'Message from '.$field_name;
+
+$body_message = 'From: '.$field_name."\r\n";
+$body_message .= 'E-mail: '.$field_email."\r\n";
+$body_message .= 'Subject: '.$field_subject."\r\n";
+$body_message .= 'Message: '.$field_message."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message);
+
+if ($mail_status) { ?>
+  <script language="javascript" type="text/javascript">
+  alert('Thank you! Your message has been sent.');
+  window.location = 'contact.html';
+  </script>
+
+
+  <?php
+}
+
+else { ?>
+  <script language="javascript" type="text/javascript">
+  alert('Something went wrong sending your message. Please try again later.');
+  window.location = 'contact.html';
+  </script>
+
+<?php
+}
+?>
